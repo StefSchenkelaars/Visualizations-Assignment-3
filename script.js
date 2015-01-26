d1d3840b0279df1a04134c5bf00af74cb847c342
Stef Schenkelaars <stef.schenkelaars@gmail.com>
2015-01-26 14:50:23 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+var Settings = function() {
+  this.Renderer = 'edge';
+  this.Slice = 0.5;
+  this.BOOM = function(){
+    alert('BOOM!');
+  }
+  this.Threshold = 0.0;
+};
+
+var gui;
+
+window.onload = function() {
+  var settings = new Settings();
+  gui = new dat.GUI();
+
+  var renderer = gui.add(settings, 'Renderer', ['edge', 'slicer', 'opacity']);
+  renderer.onChange(function(value) {
+    $('#volume_container').append( $('#transform > VolumeData') );
+    $('#transform').append( $('#' + value) );
+
+    edge_folder.close();
+    slicer_folder.close();
+    opacity_folder.close();
+    eval(value + '_folder').open();
+  });
+
+  var edge_folder = gui.addFolder('Edge Options');
+  var edge_threshold = edge_folder.add(settings, 'Threshold', 0.0, 100.0);
+  edge_threshold.onChange(function(value){
+    $('#edge > EdgeEnhancementVolumeStyle').attr('gradientThreshold', value);
+  });
+
+  var slicer_folder = gui.addFolder('Slicer Options');
+  var slicer_position = slicer_folder.add(settings, 'Slice', 0.1, 0.78);
+  slicer_position.onChange(function(value){
+    $('#slicer MPRVolumeStyle').attr('positionLine', value);
+  });
+
+  var opacity_folder = gui.addFolder('Opacity Options');
+  opacity_folder.add(settings, 'BOOM');
+}
